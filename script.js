ae0787ff438ce80c4692015764714d007f265eb7
inviHawkz <inviHawkz@gmail.com>
2024-02-26 14:53:07 +0700

    Add basic UI, rework the code poorly

--- script.js
+++ script.js
@@ -1,75 +1,63 @@
-// Create a game of Rock-Paper-Scissors, player vs computer
-// Player input 1 of 3 choices: rock, paper, scissors
-// Computer randomly choose between 3 choices
-// Compare choices of player vs choices of computer
-// Show who won, or a tie
-// if player enter a string that is not either "rock", "paper" or "scissors"
-//      the system will tell the player to correct their input
-// checking the score of player and computer
-//      if there is a string of "won" inside the result variable, increase playerScore by one
-//      if there is a string of "lose" inside the result variable, increase computerScore by one
-//      use the string method string.includes() to look if there is "win" or "lose" in the result, it returns a boolean value
+const rock = document.querySelector('#rock');
+const paper = document.querySelector('#paper');
+const scissors = document.querySelector('#scissors');
+const container = document.querySelector('#container');
+const div = document.querySelector('div');
+const compareScore = document.createElement('div');
+const newDiv = document.createElement('div');
 
-const choices = ["rock", "paper", "scissors"];
+rock.addEventListener('click', playRound);
+paper.addEventListener('click', playRound);
+scissors.addEventListener('click', playRound);
+
 let playerScore = 0;
 let computerScore = 0;
-let round = 0;
+let round = 1;
 
-// This is the loop
-for (let i = 1; i <= 5; i++) {
-    ++round;
-    let playerChoice = prompt(`Round ${round} - Please choose between Rock, Paper or Scissors!`).toLowerCase();
-
-    // This conditional statement check if the player's input is right
-    // *****This need to keep asking the player to input the right answer until it is right, currently it is only asking once
-    //******this code isnt acting as i think, i will revisit this later
-    // if (playerChoice !== "rock" || "paper" || "scissors") {
-    //     playerChoice = prompt("Your choice is Wrong! - Please choose between Rock, Paper or Scissors!").toLowerCase();
-    // } 
-    
-    // This function return a number between 0-2 
+function playRound(e) {
+    const choices = ["rock", "paper", "scissors"];
+    let playerChoice = e.target.id;
     function computerChoice() {
         random = Math.floor(Math.random() * choices.length);
         return computerChoice = choices[random];
     }
-    console.log(`Round ${round} - Computer choose ` + computerChoice());
+    computerChoice();
 
-    // This function compare playerChoice and computerChoice
-    function playRound() {
-        if (playerChoice === computerChoice) {
-            result = "It is a tie";
-        } else if (playerChoice === "rock" && computerChoice === "paper") {
-            result = "You lose, paper beats rock";
-        } else if (playerChoice === "rock" && computerChoice === "scissors") {
-            result = "You win, rock beats scissor";
-        } else if (playerChoice === "paper" && computerChoice === "rock") {
-            result = "You win, paper beats rock";
-        } else if (playerChoice === "paper" && computerChoice === "scissors") {
-            result = "You lose, scissors beats paper";
-        } else if (playerChoice === "scissors" && computerChoice === "rock") {
-            result = "You lose, rock beats scissors";
-        } else if (playerChoice === "scissors" && computerChoice === "paper") {
-            result = "You win, scissors beats paper";
-        }
-        return result;
+    if (playerChoice === computerChoice) {
+        result = "It is a tie";
+    } else if (playerChoice === "rock" && computerChoice === "paper") {
+        result = "You lose, paper beats rock";
+    } else if (playerChoice === "rock" && computerChoice === "scissors") {
+        result = "You win, rock beats scissor";
+    } else if (playerChoice === "paper" && computerChoice === "rock") {
+        result = "You win, paper beats rock";
+    } else if (playerChoice === "paper" && computerChoice === "scissors") {
+        result = "You lose, scissors beats paper";
+    } else if (playerChoice === "scissors" && computerChoice === "rock") {
+        result = "You lose, rock beats scissors";
+    } else if (playerChoice === "scissors" && computerChoice === "paper") {
+        result = "You win, scissors beats paper";
     }
-    console.log(playRound());
 
-    // This conditional statement keeps track of the score
     if (result.includes("win")) {
         ++playerScore;
     } else if (result.includes("lose")) {
         ++computerScore;
+    } else if (result.includes("tie")) {
+        ++playerScore;
+        ++computerScore;
     }
-    console.log(`Player ${playerScore} - Computer ${computerScore}`);
-}
+    newDiv.textContent = `Round ${round}: ${result}`;
+    compareScore.textContent = `Computer ${computerScore} - You ${playerScore}`;
+    div.appendChild(newDiv);
+    div.appendChild(compareScore);
+    round++;
 
-// This conditional statement evaluate the score and declare the winner
-if (playerScore > computerScore) {
-    finalResult = "------Congratulation! You have won------";
-} else if (playerScore < computerScore) {
-    finalResult = "------Sorry, you lose------"
-} else {
-    finalResult = "------Well, you both have won------"
+    if (playerScore === 5) {
+        newDiv.textContent = "-----You Are The Winner!!!-----";
+        div.removeChild(compareScore);
+    } else if (computerScore === 5) {
+        newDiv.textContent = "-----You Lose!!!-----";
+        div.removeChild(compareScore);
+    }
 }
-console.log(finalResult);
